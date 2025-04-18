13202a06ef8cc3db452889a14b7f4e4643d657c2
Mecha192 <Meka192@yahoo.com>
2025-04-18 13:16:03 -0400

    0.0.1 Initial Creation

---
+++ main.py
@@ -0,0 +1,20 @@
+# allows the use of the pygame library
+import pygame
+import constants
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
